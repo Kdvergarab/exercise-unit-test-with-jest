dfeb41a67c8777253aee06b8d53c10672d4286d5
Kdvergarab <kdvergarab@gmail.com>
2022-03-28 14:47:44 +0000

    avance

---
+++ app.js
@@ -0,0 +1,43 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// let oneEuroIs = {
+//     "JPY": 127.9, // japan yen
+//     "USD": 1.2, // us dollar
+//     "GBP": 0.8, // british pound
+// }
+// // 1 EUR (Euro) = 1.2 USD (US Dollar)
+
+// fromDollarToYen
+
+// fromEuroToDollar 
+
+// fromYanToPound
+
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    // convertimos el valor a dolares
+    let valueInYen = valueInDollar * 127.9;
+    // retornamos el valor
+    return valueInYen;
+}
+
+
+const fromYenToPound = function(valueInYen){
+    // convertimos el valor a dolares
+    let valueInPound = valueInYen * 0.8;
+    // retornamos el valor
+    return valueInPound;
+}
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
---
+++ test.js
@@ -0,0 +1,21 @@
+const { sum } = require('./app.js');
+// comienza tu primera prueba
+
+test('adds 14 + 9 to equal 23', () => {
+    //dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+    // esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+test("One euro should be 1.206 dollars", function(){
+    const { fromEuroToDollar } = require('./app.js')
+    expect(fromEuroToDollar(3.5)).toBe(4.2)});
+
+test("One dollars should be 153.48 yen", function(){
+        const { fromDollarToYen } = require('./app.js')
+        expect(fromDollarToYen(6)).toBe(767.4000000000001)});
+
+        test("One Yen should be 122.784 Pound", function(){
+            const { fromYenToPound } = require('./app.js')
+            expect(fromYenToPound(1)).toBe(122.784)});
+        
